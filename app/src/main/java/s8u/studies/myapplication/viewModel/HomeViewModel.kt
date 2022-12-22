64f02f9a2257c353b18b1d0cddef5d0f22e2fb12
EndrylFiorotti <endryl.fiorotti@sptech.school>
2022-12-22 15:51:54 -0300

    Loading na tela home

--- app/src/main/java/s8u/studies/myapplication/viewModel/HomeViewModel.kt
+++ app/src/main/java/s8u/studies/myapplication/viewModel/HomeViewModel.kt
@@ -12,35 +12,45 @@ import s8u.studies.myapplication.model.Pokedex.PokedexEntries
 import s8u.studies.myapplication.model.Pokemon.PokemonTypeEnd
 import s8u.studies.myapplication.repository.PokedexRepository
 
-class HomeViewModel(private val pokedex : PokedexRepository) : ViewModel() {
+class HomeViewModel(private val pokedex: PokedexRepository) : ViewModel() {
     private var _listPokedexEntriesLiveData = MutableLiveData<ArrayList<PokedexEntries>>()
-    val listPokedexEntriesLiveData: LiveData<ArrayList<PokedexEntries>> = _listPokedexEntriesLiveData
+    val listPokedexEntriesLiveData: LiveData<ArrayList<PokedexEntries>> =
+        _listPokedexEntriesLiveData
 
     private var _listPokedexTypesLiveData = MutableLiveData<ArrayList<PokemonTypeEnd>>()
     val listPokedexTypesLiveData: LiveData<ArrayList<PokemonTypeEnd>> = _listPokedexTypesLiveData
 
+    private var _loadingPokeballTrue = MutableLiveData<Unit>()
+    val loadingPokeballTrue: LiveData<Unit> = _loadingPokeballTrue
 
-    fun getPokedexEntriesList () {
+    private var _loadingPokeballFalse = MutableLiveData<Unit>()
+    val loadingPokeballFalse: LiveData<Unit> = _loadingPokeballFalse
+
+
+    fun getPokedexEntriesList() {
         viewModelScope.launch {
             _listPokedexEntriesLiveData.postValue(pokedex.getPokedex().entriesList)
         }
     }
-    fun getPokedexTypesList () {
+
+    fun getPokedexTypesList() {
         val typeList = arrayListOf<PokemonTypeEnd>()
         val api = RetrofitObject.createNetworkService<PokemonTypeEndpoint>()
         viewModelScope.launch {
-                for(i in 0 until listPokedexEntriesLiveData.value!!.size){
-                    Log.i("DST", i.toString())
-                    typeList.add(api.getPokemon(listPokedexEntriesLiveData.value!![i].id.toString()))
+            for (i in 0 until listPokedexEntriesLiveData.value!!.size) {
+                Log.i("DST", i.toString())
+                typeList.add(api.getPokemon(listPokedexEntriesLiveData.value!![i].id.toString()))
 
-                    if(i == listPokedexEntriesLiveData.value!!.size - 1){
-                        _listPokedexTypesLiveData.postValue(typeList)
-                    }
+                if (i == listPokedexEntriesLiveData.value!!.size - 1) {
+                    _listPokedexTypesLiveData.postValue(typeList)
                 }
-
-
+            }
         }
+    }
 
+    fun setLoadingState(isVisible: Boolean) {
+        if (isVisible) _loadingPokeballTrue.postValue(Unit)
+        else _loadingPokeballFalse.postValue(Unit)
     }
 
 }
