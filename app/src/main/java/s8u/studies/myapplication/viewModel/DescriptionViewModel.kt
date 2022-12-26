f55e191ff04e46a50401cc50f6340c765f23dc7c
EndrylFiorotti <endryl.fiorotti@sptech.school>
2022-12-26 10:30:50 -0300

    Cores no tipo do pokémon na lista de pokémons

--- app/src/main/java/s8u/studies/myapplication/viewModel/DescriptionViewModel.kt
+++ app/src/main/java/s8u/studies/myapplication/viewModel/DescriptionViewModel.kt
@@ -70,30 +70,6 @@ class DescriptionViewModel : ViewModel() {
         if (size == 2) behavior()
     }
 
-    fun colorBackgroundType(nameType: String) : Int {
-        return when (nameType) {
-            "bug" -> R.color.bug
-            "dark" -> R.color.dark
-            "dragon" -> R.color.dragon
-            "electric" -> R.color.electric
-            "fairy" -> R.color.fairy
-            "fighting" -> R.color.fighting
-            "fire" -> R.color.fire
-            "flying" -> R.color.flying
-            "ghost" -> R.color.ghost
-            "grass" -> R.color.grass
-            "ground" -> R.color.ground
-            "ice" -> R.color.ice
-            "normal" -> R.color.normal
-            "poison" -> R.color.poison
-            "psychic" -> R.color.psychic
-            "rock" -> R.color.rock
-            "steel" -> R.color.steel
-            "water" -> R.color.water
-            else -> R.color.light_gray_2
-        }
-    }
-
     fun setLoadingState(isVisible: Boolean) {
         if (isVisible) _loadingPokeballTrue.postValue(Unit)
         else _loadingPokeballFalse.postValue(Unit)
