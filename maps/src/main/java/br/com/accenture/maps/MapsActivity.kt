ed18ed0cdc0415c092a4a0fcf624adbfb21329ea
vinicius-s8u <vinicius.barbosa@sptech.school>
2023-01-10 13:16:17 -0300

    Teste com angulos e inclinações do Zoom

--- maps/src/main/java/br/com/accenture/maps/MapsActivity.kt
+++ maps/src/main/java/br/com/accenture/maps/MapsActivity.kt
@@ -7,12 +7,11 @@ import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import br.com.accenture.maps.databinding.ActivityMapsBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -35,23 +34,43 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isCompassEnabled = false
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.custom_map))
         setUpMap()
     }
 
     @SuppressLint("MissingPermission")
     private fun setUpMap() {
+
+        val task = fusedLocationProviderClient.lastLocation
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
             != PackageManager.PERMISSION_GRANTED
-        ) return
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),101)
+           return
+        }
         mMap.isMyLocationEnabled = true
-        fusedLocationProviderClient.lastLocation.addOnSuccessListener(this) {
+        task.addOnSuccessListener(this) {
             if (it != null) {
                 currentLocation = it
                 val currentLatLong = LatLng(it.latitude, it.longitude)
                 mMap.addMarker(MarkerOptions().position(currentLatLong).title("My Position"))
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLong))
+
+                mMap.setOnMapLoadedCallback {
+                    val cameraPosition = CameraPosition.builder()
+                        .target(currentLatLong)
+                        .zoom(18f)
+                        .bearing(270f)
+                        .tilt(70f)
+                        .build()
+
+                  mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                }
             }
         }
     }
